0c5c83e3fa76c5083bccb7a529fca87597c4adea
jjuchoa <jbfdujunior@gmail.com>
2019-05-27 15:37:33 -0300

    Ajustes no menu

--- cadastro_integrado/urls.py
+++ cadastro_integrado/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.contrib import admin
 
 from pais.views import PaisListView, PaisCreateView, PaisUpdateView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', PaisListView.as_view(), name='pais_list'),
+    path('', TemplateView.as_view(template_name="pais/home.html"), name='home'),
+    path('list/', PaisListView.as_view(), name='pais_list'),
     path('add/', PaisCreateView.as_view(), name='pais_add'),
     path('<int:pk>/edit/', PaisUpdateView.as_view(), name='pais_edit'),
     path('admin/', admin.site.urls),
